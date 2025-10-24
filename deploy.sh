46132453c76c4f37a08ef4c92f73d1a80a5545f8
msteinborn1 <mmullo01@tufts.edu>
2025-10-24 17:53:06 -0400

    Update source

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# 1. Build the site into public/
+hugo
+
+# 2. Commit and push source (main branch)
+git add .
+git commit -m "Update source"
+git push origin main
+
+# 3. Push the generated site to gh-pages
+cd public
+git add .
+git commit -m "Deploy site"
+git push origin gh-pages
+cd ..
+
